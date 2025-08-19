c7b28525cd22ed556ca8438e348b52e86349d0d7
lioka0099 <lioka009@gmail.com>
2025-08-19 13:41:21 +0300

    Fixed bug

--- client/src/pages/HistoryTripDetails.jsx
+++ client/src/pages/HistoryTripDetails.jsx
@@ -24,20 +24,20 @@ export default function HistoryTripDetails() {
 
   // Fetch trip by ID on mount
   useEffect(() => {
-    let isMounted = true;
+    // let isMounted = true;
     async function fetchTrip() {
       try {
         const { data } = await api.get(`/trips/${tripId}`);
-        if (!isMounted) return;
+        // if (!isMounted) return;
         setTrip(data);
       } catch {
         window.alert("Error fetching trip.");
       }
     }
     fetchTrip();
-    return () => {
-      isMounted = false;
-    };
+    // return () => {
+    //   // isMounted = false;
+    // };
   }, [tripId, api]);
 
   return (
